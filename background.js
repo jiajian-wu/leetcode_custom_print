e47fab18781efe281aa37029847af1f61b549960
jiajian_wu <jaywu.quant@gmail.com>
2021-01-16 11:42:56 -0500

    final

--- background.js
+++ background.js
@@ -16,9 +16,3 @@ chrome.runtime.onInstalled.addListener(function () {
     });
 
 });
-
-//
-//
-// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-//    chrome.tabs.executeScript(null,{file:"content.js"});
-// });
